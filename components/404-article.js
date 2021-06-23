4f9402d0f6b3c100be37df9f0f3b6f10ec9157b3
Akshat Agarwal <humancalico@disroot.org>
2021-06-23 21:52:46 +0530

    component: add a 404-article component

---
+++ components/404-article.js
@@ -0,0 +1,28 @@
+import Link from '@/components/Link'
+
+export default function FourZeroFourArticle() {
+  return (
+    <div className="flex flex-col items-start justify-start md:justify-center md:items-center md:flex-row md:space-x-6 md:mt-24">
+      <div className="pt-6 pb-8 space-x-2 md:space-y-5">
+        <h1 className="text-6xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 md:text-8xl md:leading-14 md:border-r-2 md:px-6">
+          404
+        </h1>
+      </div>
+      <div className="max-w-md">
+        <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
+          Sorry no post was found on this page.
+        </p>
+        <p className="mb-8">
+          It seems like the author hasn't written any posts yet either because he feels like he has
+          nothing unique to say or he is just being lazy. He thought that buying a domain would give
+          him a little push to at least get started but alas he thought wrong.
+        </p>
+        <Link href="/about">
+          <button className="inline px-4 py-2 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg shadow focus:outline-none focus:shadow-outline-blue hover:bg-blue-700 dark:hover:bg-blue-500">
+            Go to the about page
+          </button>
+        </Link>
+      </div>
+    </div>
+  )
+}
